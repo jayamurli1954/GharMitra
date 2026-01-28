05d800896859d317d33e507438d7b3b78ce4d588
Muralidhar <muralidhar@example.com>
2026-01-28 20:25:48 +0530

    Allow JSX in .js files for Vite

--- web/vite.config.js
+++ web/vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react({
+      include: '**/*.{js,jsx,ts,tsx}',
+    }),
+  ],
   server: {
     port: 3006,
     host: 'localhost',
